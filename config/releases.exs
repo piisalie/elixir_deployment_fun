41e5f82735b6110f5cc8b465fc7b2eabfa0b8079
Paul Dawson <paul@into.computer>
2020-04-16 18:59:41 -0500

    fix endpoint

--- config/releases.exs
+++ config/releases.exs
@@ -1,2 +1,8 @@
 import Config
-config :my_app, :secret_key, System.fetch_env!("MY_APP_SECRET_KEY")
+secret_key_base = System.fetch_env!("MY_APP_SECRET_KEY")
+
+config :deployment_example, DeploymentExampleWeb.Endpoint, server: true
+
+config :deployment_example, DeploymentExampleWeb.Endpoint,
+  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  secret_key_base: secret_key_base
